2f1e3a3b08ff87ab346eb689b7b20a0ef6700b38
Robertooan07 <RM551460@fiap.com.br>
2023-04-13 20:56:26 -0300

    para mostrar a mensagem se não Preencher os Campos

--- Js/script.js
+++ Js/script.js
@@ -5,17 +5,30 @@
 
     
 //Convertendo a lista de eobjetos HMTL para Array com SPREAD
-const inputs = [...document.getElementsByTagName("input")];
+// const inputs = [...document.getElementsByTagName("input")];
 
-//Listanto os elementos da lista com FOR-EACH
-inputs.forEach((eLemento)=>{                       //A cada iteração ele vai gerar um elemento
-    //Utilizando o elemento que estava na lista
-    console.log(eLemento);
-}) 
-//innerHTML: "" ele recebe HTML, pode simplismente colocar uma tag    innerText: "" ele vai pegar somente texto
+// //Listanto os elementos da lista com FOR-EACH
+// inputs.forEach((eLemento)=>{                       //A cada iteração ele vai gerar um elemento
+//     //Utilizando o elemento que estava na lista
+//     console.log(eLemento);
+// }) 
+// //innerHTML: "" ele recebe HTML, pode simplismente colocar uma tag    innerText: "" ele vai pegar somente texto
 
-function soma(){
-    n1 = 10;
-    n2 = 5;
-    console.log(`Resultado da soma : ${n1+n2}`) //isso é interpolação, 
+// function soma(){
+//     n1 = 10;
+//     n2 = 5;
+//     console.log(`Resultado da soma : ${n1+n2}`) //isso é interpolação, 
+// }
+
+function validacao(){
+    const inputs = [...document.getElementsByTagName("input")];
+
+    inputs.forEach((eLemento)=>{
+        if(eLemento.innerText ==""){
+            alert("PREENCHA TODOS OS CAMPOS")
+            return;
+        }
+
+    }) 
+
 }
