959681572f3b8e4d9d4b59c56f6a77cca6e757c6
Robertooan07 <RM551460@fiap.com.br>
2023-04-13 20:46:34 -0300

    iterpolação e soma

--- Js/script.js
+++ Js/script.js
@@ -1,2 +1,21 @@
 //isso vai fazer com  que ele retorne mais de um elemento
-const imputs = document.getElementsByTagName
+//aqui estamos retornando uma lista de HTML-Objects
+
+// const inputs = document.getElementsByTagName("input");
+
+    
+//Convertendo a lista de eobjetos HMTL para Array com SPREAD
+const inputs = [...document.getElementsByTagName("input")];
+
+//Listanto os elementos da lista com FOR-EACH
+inputs.forEach((eLemento)=>{                       //A cada iteração ele vai gerar um elemento
+    //Utilizando o elemento que estava na lista
+    console.log(eLemento);
+}) 
+//innerHTML: "" ele recebe HTML, pode simplismente colocar uma tag    innerText: "" ele vai pegar somente texto
+
+function soma(){
+    n1 = 10;
+    n2 = 5;
+    console.log(`Resultado da soma : ${n1+n2}`) //isso é interpolação, 
+}
